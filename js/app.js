fe1385663b719951773b9bd8939e5e93aa36dd3b
jorge <julianacero98@gmail.com>
2019-01-02 01:18:20 -0600

    submitExpense method created and parcialy coded

--- js/app.js
+++ js/app.js
@@ -58,8 +58,43 @@ class UI {
         this.expenseFeedback.classList.remove('showItem');
       }, 4000);
     } else {
-      
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = '';
+      this.amountInput.value = '';
+
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount
+      }
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      // show balance 
     }
+  }
+
+  // add expense
+  addExpense(expense){
+    const div = document.createElement('div');
+    div.classList.add('expense');
+    div.innerHTML = `<div class="expense-item d-flex justify-content-between align-items-baseline">
+
+    <h6 class="expense-title mb-0 text-uppercase list-item">${expense.title}</h6>
+    <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+    <div class="expense-icons list-item">
+
+     <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+      <i class="fas fa-edit"></i>
+     </a>
+     <a href="#" class="delete-icon" data-id="${expense.id}">
+      <i class="fas fa-trash"></i>
+     </a>
+    </div>
+   </div>`;
+
+   this.expenseList.appendChild(div);
   }
 
   // total expense
@@ -86,6 +121,7 @@ function eventListeners() {
   // expense form submit
   expenseForm.addEventListener('submit', event => {
     event.preventDefault();
+    ui.submitExpenseForm();
   });
 
   // expense click
